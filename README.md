d035a3aef7ab1ee32772d4d3fd4caca9855e0e78
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-04 12:35:55 -0300

    Adding useState for count simple

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-hooks
 Introduction in React Hooks
+
+- Lembrete de anotações feitas pelo site oficial do React.
+
+
--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+  
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-      teste
+      <p>Você clicou {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Clicke me
+      </button>
     </div>
   );
 }
