095e26df93769d092513d840d5084d0d23fe4bf0
Patrik Matos <patrikmatos.tecinfo@gmail.com>
2020-03-09 14:52:56 -0300

    Update css

--- src/View/HomePage/index.js
+++ src/View/HomePage/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Page from '../../components/Page';
 import Content from '../../components/Content';
+import styled from 'styled-components';
+
+const S = {
+  
+  Container: styled.div`
+   
+  `,
+
+  Button: styled.button`
+    padding: 10px 25px;
+    color: #fafafa;
+    font-size: 14px;
+    
+  `,
+}
+
+
 
 export default function HomePage(){
   return (
     <Page>
       <Content>
-        <button>Funções</button>
-        <button>Classes</button>
+          <S.Container>
+            <S.Button>Funções</S.Button>
+            <S.Button>Classes</S.Button>
+          </S.Container>
       </Content>
     </Page>
   );
--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   margin: auto;
   max-width: 1000px;
+  height: 500px;
   text-align: center;
   background: #FAFAFA;
 `;
